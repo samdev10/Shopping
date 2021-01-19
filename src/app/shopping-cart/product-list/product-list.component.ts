3603e7ec20f612a6b57a5dbf685a9f0f36507eb9
samdev10 <santhoshn1099@outlook.com>
2021-01-18 20:58:14 -0500

    Set up mock response

--- src/app/shopping-cart/product-list/product-list.component.ts
+++ src/app/shopping-cart/product-list/product-list.component.ts
@@ -12,12 +12,28 @@ export class ProductListComponent implements OnInit {
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe((response) => {
-      response.data.forEach((product) => {
-        this.productList.push(
-          new Product(product.id, product.title, product.summary, product.price)
-        );
-      });
+    this.getMockResponse().forEach((product) => {
+      this.productList.push(
+        new Product(
+          product.id,
+          product.name,
+          product.description,
+          product.price
+        )
+      );
     });
+  }
+
+  getMockResponse(): Product[] {
+    return [
+      new Product(1, 'Java Programmer', 'Core Java', 20),
+      new Product(2, 'Javascript', 'Javascript', 30),
+      new Product(3, 'Design Patterns', 'Design Patterns', 40),
+      new Product(4, 'React Js', 'React Js', 50),
+      new Product(5, 'Angular', 'Angular', 60),
+      new Product(6, 'Spring Framework', 'Spring Framework', 70),
+      new Product(7, 'Docker', 'Docker', 20),
+      new Product(8, 'Mysql', 'Mysql', 20),
+    ];
   }
 }
